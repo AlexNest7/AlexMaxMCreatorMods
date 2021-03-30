753e6ccb19c1b32a161e02a482037325bd8ad889
AlexNest7 <Vilexma@Gmail.com>
2021-03-30 14:14:04 +0300

    Re-added a seed salsd and a chopping hammer.

---
+++ src/main/java/net/mcreator/alexnestsfoodmod/item/SeedSaladItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.alexnestsfoodmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.alexnestsfoodmod.FoodModModElements;
+
+import java.util.List;
+
+@FoodModModElements.ModElement.Tag
+public class SeedSaladItem extends FoodModModElements.ModElement {
+	@ObjectHolder("food_mod:seed_salad")
+	public static final Item block = null;
+	public SeedSaladItem(FoodModModElements instance) {
+		super(instance, 9);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f).build()));
+			setRegistryName("seed_salad");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Even though ingredients are odd..."));
+			list.add(new StringTextComponent("It looks tasty."));
+		}
+	}
+}
