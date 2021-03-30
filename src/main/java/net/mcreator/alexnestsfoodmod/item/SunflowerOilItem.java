0d7addcb4ca7cd10122b1d831a7aa26341fef198
Kekthicc <Kekthicc@DESKTOP-7EMB2CC>
2021-03-30 13:28:38 +0300

    added sunflower oil

---
+++ src/main/java/net/mcreator/alexnestsfoodmod/item/SunflowerOilItem.java
@@ -0,0 +1,70 @@
+
+package net.mcreator.alexnestsfoodmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.alexnestsfoodmod.FoodModModElements;
+
+import java.util.List;
+
+@FoodModModElements.ModElement.Tag
+public class SunflowerOilItem extends FoodModModElements.ModElement {
+	@ObjectHolder("food_mod:sunflower_oil")
+	public static final Item block = null;
+	public SunflowerOilItem(FoodModModElements instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(16).rarity(Rarity.COMMON));
+			setRegistryName("sunflower_oil");
+		}
+
+		@Override
+		public boolean hasContainerItem() {
+			return true;
+		}
+
+		@Override
+		public ItemStack getContainerItem(ItemStack itemstack) {
+			return new ItemStack(Items.GLASS_BOTTLE, (int) (1));
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Useful for frying and making salads."));
+		}
+	}
+}
