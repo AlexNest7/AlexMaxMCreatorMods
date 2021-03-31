ff0d8143cfc5f6fa9eadc58b8fb59b1782f0b526
AlexNest7 <Vilexma@Gmail.com>
2021-03-31 10:05:08 +0300

    I dunno what i did.

--- src/main/java/net/mcreator/alexnestsfoodmod/item/ChoppingHammerItem.java
+++ src/main/java/net/mcreator/alexnestsfoodmod/item/ChoppingHammerItem.java
@@ -1,24 +1,35 @@
 
 package net.mcreator.alexnestsfoodmod.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.alexnestsfoodmod.FoodModModElements;
+
+import java.util.List;
+
 @FoodModModElements.ModElement.Tag
 public class ChoppingHammerItem extends FoodModModElements.ModElement {
-
 	@ObjectHolder("food_mod:chopping_hammer")
 	public static final Item block = null;
-
 	public ChoppingHammerItem(FoodModModElements instance) {
 		super(instance, 30);
-
 	}
 
 	@Override
 	public void initElements() {
 		elements.items.add(() -> new ItemCustom());
 	}
-
 	public static class ItemCustom extends Item {
-
 		public ItemCustom() {
 			super(new Item.Properties().group(ItemGroup.MISC).maxDamage(60).rarity(Rarity.COMMON));
 			setRegistryName("chopping_hammer");
@@ -59,7 +70,5 @@ public class ChoppingHammerItem extends FoodModModElements.ModElement {
 			super.addInformation(itemstack, world, list, flag);
 			list.add(new StringTextComponent("A hammer used for chopping meat."));
 		}
-
 	}
-
 }
