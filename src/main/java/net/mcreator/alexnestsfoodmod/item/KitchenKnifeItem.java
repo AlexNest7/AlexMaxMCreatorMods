550a4614ed767aab317e3333f062f5ff5c2fe21a
AlexNest7 <Vilexma@Gmail.com>
2021-03-31 11:47:29 +0300

    Added a kitchen knife.

---
+++ src/main/java/net/mcreator/alexnestsfoodmod/item/KitchenKnifeItem.java
@@ -0,0 +1,78 @@
+
+package net.mcreator.alexnestsfoodmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.alexnestsfoodmod.itemgroup.AMFoodModItemGroup;
+import net.mcreator.alexnestsfoodmod.FoodModModElements;
+
+import java.util.List;
+
+@FoodModModElements.ModElement.Tag
+public class KitchenKnifeItem extends FoodModModElements.ModElement {
+	@ObjectHolder("food_mod:kitchen_knife")
+	public static final Item block = null;
+	public KitchenKnifeItem(FoodModModElements instance) {
+		super(instance, 34);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 180;
+			}
+
+			public float getEfficiency() {
+				return 5f;
+			}
+
+			public float getAttackDamage() {
+				return 1.2f;
+			}
+
+			public int getHarvestLevel() {
+				return 2;
+			}
+
+			public int getEnchantability() {
+				return 5;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -2.2f, new Item.Properties().group(AMFoodModItemGroup.tab)) {
+			@Override
+			public boolean hasContainerItem() {
+				return true;
+			}
+
+			@Override
+			public ItemStack getContainerItem(ItemStack itemstack) {
+				ItemStack retval = new ItemStack(this);
+				retval.setDamage(itemstack.getDamage() + 1);
+				if (retval.getDamage() >= retval.getMaxDamage()) {
+					return ItemStack.EMPTY;
+				}
+				return retval;
+			}
+
+			@Override
+			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+				super.addInformation(itemstack, world, list, flag);
+				list.add(new StringTextComponent("It's pretty sharp."));
+			}
+		}.setRegistryName("kitchen_knife"));
+	}
+}
